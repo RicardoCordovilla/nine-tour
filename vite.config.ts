b66436fc22671be183641649898680d2929f4053
RicardoCordovilla <ricardocordovillaf@gmail.com>
2025-02-20 02:41:19 -0500

    LAN live server

--- vite.config.ts
+++ vite.config.ts
@@ -3,5 +3,9 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [react(),],
+  server: {
+    host: true, // Use 'true' to expose to network OR use a specific IP
+    port: 5173
+  }
 })
