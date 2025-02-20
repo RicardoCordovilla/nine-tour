48546a6e7fd946495b136db47dd4b7d212bd5c0f
RicardoCordovilla <ricardocordovillaf@gmail.com>
2025-02-20 16:02:56 -0500

    optimize images loading

--- src/pages/tour/TourPage.tsx
+++ src/pages/tour/TourPage.tsx
@@ -1,14 +1,38 @@
-import { useState } from "react";
-import { useParams, useNavigate } from "react-router-dom";
-import { motion, AnimatePresence } from "framer-motion";
-import tourRoutes from "../../routes/tour-toutes";
+import { useEffect, useMemo, useState } from "react";
+import { useNavigate, useParams } from "react-router-dom";
+import tourRoutes from "../../routes/tour-routes";
 import "./TourPage.css";
 
 const TourPage = () => {
     const { id } = useParams<{ id: string }>();
     const navigate = useNavigate();
-    const images = tourRoutes[id || ""] || [];
+    const images = useMemo(() => tourRoutes[id || ""] || [], [id]);
     const [step, setStep] = useState(0);
+    const [isLoading, setIsLoading] = useState(true);
+
+    useEffect(() => {
+        if (images.length > 0) {
+            let loaded = 0;
+            const tempLoadedImages: string[] = [];
+
+            // Precargar imágenes
+            images.forEach((image) => {
+                const img = new Image();
+                img.src = image;
+                img.onload = () => {
+                    loaded++;
+                    tempLoadedImages.push(image);
+
+                    // Cuando todas las imágenes estén cargadas, ocultar el loader
+                    if (loaded === images.length) {
+                        setIsLoading(false);
+                    }
+                };
+            });
+        } else {
+            setIsLoading(false);
+        }
+    }, [images]);
 
     const handleNext = () => {
         if (step < images.length - 1) setStep(step + 1);
@@ -25,57 +49,57 @@ const TourPage = () => {
     return (
         <div className="w-full h-screen flex flex-col items-center justify-center relative">
             <h1 className="text-2xl font-bold text-center mt-4">Departamento {id}</h1>
-            <div className="w-full flex flex-col items-center justify-center bg relative">
-                {images.length > 0 ? (
-                    <div className="w-full flex flex-col items-center relative mt-4">
-                        <div className="relative w-[400px] h-[300px] flex items-center justify-center">
-                            <AnimatePresence mode="wait">
-                                <motion.img
-                                    key={step} // Para que detecte cambios y haga la animación
-                                    src={images[step]}
-                                    alt={`Paso ${step}`}
-                                    className="absolute object-cover rounded-md"
-                                    initial={{ opacity: 0, scale: 0.8 }}
-                                    animate={{ opacity: 1, scale: 1 }}
-                                    exit={{ opacity: 0, scale: 1.2 }}
-                                    transition={{ duration: 0.5, ease: "easeInOut" }}
-                                />
-                            </AnimatePresence>
-                        </div>
 
-                        {/* Botón atrás */}
-                        <button
-                            className="absolute bottom-4 left-1/2 transform -translate-x-1/2 text-white bg-slate-800 bg-opacity-80
-                        px-4 py-2 rounded-md disabled:opacity-50"
-                            onClick={handlePrev}
-                            disabled={step === 0}
-                        >
-                            Atrás
-                        </button>
-
-                        {/* Botón siguiente / finalizar */}
-                        <div className="absolute top-4 left-1/2 transform -translate-x-1/2 text-white">
-                            {step === images.length - 1 ? (
-                                <button
-                                    className="btn btn-active btn-primary mt-4"
-                                    onClick={handleFinish}
-                                >
-                                    Finalizar
-                                </button>
-                            ) : (
-                                <button
-                                    className="btn btn-active btn-neutral mt-4 bg-slate-800 bg-opacity-80 px-4 py-2 rounded-md"
-                                    onClick={handleNext}
-                                >
-                                    Adelante
-                                </button>
-                            )}
-                        </div>
+            <div className="w-full flex flex-col items-center justify-center relative">
+                {isLoading ? (
+                    // Muestra un mensaje de carga hasta que las imágenes estén precargadas
+                    <div className="flex flex-col items-center">
+                        <p className="text-lg font-semibold">Cargando imágenes...</p>
+                        <div className="loader mt-4"></div>
                     </div>
                 ) : (
-                    <p>No hay ruta para este departamento.</p>
-                )}
+                    images.length > 0 && (
+                        <div className="w-full flex flex-col items-center relative mt-4">
+                            <div className="relative w-[400px] h-[300px] flex items-center justify-center">
+                                <img
+                                    key={step}
+                                    src={images[step]}
+                                    alt={`Paso ${step}`}
+                                    className="absolute object-cover rounded-md transition-opacity duration-500"
+                                />
+                            </div>
 
+                            {/* Botón atrás */}
+                            <button
+                                className="absolute bottom-4 left-1/2 transform -translate-x-1/2 text-white bg-slate-800 bg-opacity-80
+                                px-4 py-2 rounded-md disabled:opacity-50"
+                                onClick={handlePrev}
+                                disabled={step === 0}
+                            >
+                                Atrás
+                            </button>
+
+                            {/* Botón siguiente / finalizar */}
+                            <div className="absolute top-4 left-1/2 transform -translate-x-1/2 text-white">
+                                {step === images.length - 1 ? (
+                                    <button
+                                        className="btn btn-active btn-primary mt-4"
+                                        onClick={handleFinish}
+                                    >
+                                        Finalizar
+                                    </button>
+                                ) : (
+                                    <button
+                                        className="btn btn-active btn-neutral mt-4 bg-slate-800 bg-opacity-80 px-4 py-2 rounded-md"
+                                        onClick={handleNext}
+                                    >
+                                        Adelante
+                                    </button>
+                                )}
+                            </div>
+                        </div>
+                    )
+                )}
             </div>
         </div>
     );
